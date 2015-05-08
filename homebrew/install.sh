112a27a8708f01181e702ef6db5a42c8867590e1
Kim Thoenen <kim@smuzey.ch>
2015-05-09 01:05:20 +0200

    homebrew: update & upgrade if already installed

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,11 +8,18 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
+  echo "  Installing Homebrew."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "  Upgrading Homebrew."
+  brew update
+  echo "  Upgrading Homebrew packages."
+  brew upgrade
 fi
 
 # Install homebrew packages
 brew install grc coreutils spark
+# Cleanup
+brew cleanup
 
 exit 0
