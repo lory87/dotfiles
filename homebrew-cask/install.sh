7f67518b0908b2582b45bb4be393b4f7f73b5714
Kim Thoenen <kim@smuzey.ch>
2015-05-09 01:05:57 +0200

    add homebrew-cask

---
+++ homebrew-cask/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Homebrew Cask
+
+# Check for Brew Cask
+if test ! $(which brew-cask.rb)
+then
+  echo "  Installing Homebrew Cask."
+  brew install caskroom/cask/brew-cask
+else
+  echo "  Upgrading Homebrew Cask."
+  brew upgrade brew-cask
+fi
+
+# Install Cask Apps
+brew cask install 1password hipchat pycharm java6 skype google-chrome macdown dropbox google-drive sublime-text rowanj-gitx
+
+brew cask cleanup
+
+exit 0
