73afbdd0081c2c4fe502b6770173cdb80da5f5ac
Kim Thoenen <kim@smuzey.ch>
2015-05-09 01:08:00 +0200

    system: add aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,12 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ls='gls -F --color'
+  alias l='gls -lAh --color'
+  alias ll='gls -l --color'
   alias la='gls -A --color'
 fi
+
+alias ip='ifconfig | grep netmask | grep -v 127.0.0.1'
+alias c='charm .'
+alias fuck='eval $(thefuck $(fc -ln -1))'
