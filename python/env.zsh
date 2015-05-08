6637a86cdc5991e71568e0decce2fe8b8e12959e
Kim Thoenen <kim@smuzey.ch>
2015-05-09 01:07:51 +0200

    python: env + aliases

---
+++ python/env.zsh
@@ -0,0 +1,3 @@
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/kim/.local/bin:/Users/kim/.envs/bins:/Users/kim/.scripts:$PATH
+export ARCHFLAGS="-arch x86_64"
+export PIP_REQUIRE_VIRTUALENV=true
