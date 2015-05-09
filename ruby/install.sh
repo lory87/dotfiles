9ddda0c8ea1e0a5d6fe24f0e13a6bcaeb544bea5
Kim Thoenen <kim@smuzey.ch>
2015-05-09 02:01:56 +0200

    ruby: add ruby & rum install

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,1 +1,15 @@
+# Install RVM + Ruby
+if test ! $(which rvm)
+then
+  echo "  Installing RVM & Ruby."
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby
+else
+  echo "  Upgrading RVM & Ruby."
+  rvm get stable --ruby
+fi
+
+# Load RVM Config
+source ~/.rvm/scripts/rvm
+
+# Install gems
 gem install lunchy
