38941e6a212d267299e9751ccba907cc3f2eb4ff
Kim Thoenen <kim@smuzey.ch>
2015-05-09 02:06:34 +0200

    python: cleanup path

--- python/env.zsh
+++ python/env.zsh
@@ -1,3 +1,2 @@
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/kim/.local/bin:/Users/kim/.envs/bins:/Users/kim/.scripts:$PATH
 export ARCHFLAGS="-arch x86_64"
 export PIP_REQUIRE_VIRTUALENV=true
